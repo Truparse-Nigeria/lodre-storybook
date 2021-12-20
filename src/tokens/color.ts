648f8a967857c269342852bd7c761cc76d06903e
Okonkwo Henry <henryokonkwo01@gmail.com>
2021-12-20 21:03:45 +0100

    first commit

---
+++ src/tokens/color.ts
@@ -0,0 +1,7 @@
+export const PALETTE = {
+    alert: '#DF2D42',
+    primary: '#FFC20E',
+    success: '#17B64B',
+    white: '#FFFFFF'
+};
+
---
+++ src/tokens/font.ts
@@ -0,0 +1,18 @@
+export const TYPOGRAPHY = {
+    type: {
+      primary: '"Montserrat", sans-serif',
+    },
+    weight: {
+      regular: '400',
+      bold: '600',
+      extrabold: '800',
+      black: '900',
+    },
+    size: {
+      s1: 13,
+      m1: 14,
+      m2: 16,
+      l1: 18,
+      l2: 20,
+    },
+  };
