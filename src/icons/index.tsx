b46b647caef7c044de8429ce32aa12dc1ada9173
Okonkwo Henry <henryokonkwo01@gmail.com>
2021-12-27 23:25:14 +0100

    updated icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -7,12 +7,7 @@ export { default as Remove } from "./Remove";
 export { default as Safe } from "./Safe";
 export { default as User } from "./User";
 export { default as EyeClose } from "./EyeClose";
-<<<<<<< HEAD
 export { default as EyeOpen } from "./EyeOpen";
 export { default as Headphone } from "./Headphone";
 export { default as HeartFill } from "./HeartFill";
-=======
-export { default as Help } from "./Help";
 export { default as ShoppingCart } from "./ShoppingCart";
-export { default as User } from "./User";
->>>>>>> f2c098c7759a3933e013485a8b4732494613f6c5
