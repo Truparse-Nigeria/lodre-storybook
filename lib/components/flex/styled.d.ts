5ca424503fe98d20840921808f362e778ce6a3fa
Okonkwo Henry <henryokonkwo01@gmail.com>
2022-01-04 11:53:35 +0100

    created lib

--- lib/components/flex/styled.d.ts
+++ lib/components/flex/styled.d.ts
@@ -1,10 +1,13 @@
+export declare type flexAlignmentType = 'start' | 'center' | 'space-evenly' | 'space-between' | 'end';
+export declare type textAlignmentType = 'left' | 'right' | 'center';
 declare type colType = {
     size?: number;
+    align?: textAlignmentType;
 };
 declare type flexType = {
     gap: number;
-    justifyContent: 'start' | 'center' | 'space-evenly' | 'space-between' | 'end';
-    alignItems: 'start' | 'center' | 'space-evenly' | 'space-between' | 'flex-end';
+    justifyContent: flexAlignmentType;
+    alignItems: flexAlignmentType;
 };
 export declare const StyledFlex: import("styled-components").StyledComponent<"div", any, flexType, never>;
 export declare const StyledCol: import("styled-components").StyledComponent<"div", any, colType, never>;
--- lib/components/index.d.ts
+++ lib/components/index.d.ts
@@ -1,6 +1,6 @@
 export { default as Navbar } from './navbar';
 export { default as Button } from './button';
-export { default as Input } from './input';
+export { default as Input, SearchInput } from './input';
 export { default as Card, ProductCard, SideProductCard } from './card';
 export { SmallText, Paragraph, Heading } from './typography';
 export { Flex, Col } from './flex';
