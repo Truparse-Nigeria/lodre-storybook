920248daf5406dce58e264a34e15d26c0bf9c3c7
Okonkwo Henry <henryokonkwo01@gmail.com>
2022-01-09 14:55:36 +0100

    update

--- lib/tokens/color.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type PaletteType = 'cream' | 'primary' | 'dark' | 'ash' | 'grey' | 'light' | 'border';
-export declare const PALETTE: {
-    [Key in PaletteType]: string;
-};
-//# sourceMappingURL=color.d.ts.map
