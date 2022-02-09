4baf6bdd25762eea7200617075d5224af702c335
Okonkwo Henry <henryokonkwo01@gmail.com>
2022-02-09 17:08:31 +0100

    updated views

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,6 @@
-import React, { FC, ReactChild, ReactChildren } from "react";
-import { StyledBottomNav, StyledGrid, StyledNavbar } from "./styled";
+import React, { FC, ReactChild, ReactChildren } from 'react';
+import { Container } from '..';
+import { StyledBottomNav, StyledGrid, StyledNavbar } from './styled';
 
 export interface INavbar {
   search?: JSX.Element;
@@ -7,17 +8,20 @@ export interface INavbar {
   logo?: JSX.Element;
   bottomNav?: JSX.Element | JSX.Element[];
   children?: ReactChild | ReactChildren | JSX.Element | JSX.Element[];
+  fluid?: boolean;
 }
 
-const Navbar: FC<INavbar> = ({ logo, nav, search, bottomNav, children }) => {
+const Navbar: FC<INavbar> = ({ logo, nav, search, bottomNav, children, fluid = false }) => {
   return (
     <div>
       <StyledNavbar>
-        <StyledGrid>
-          <div>{logo}</div>
-          {search}
-          <div>{nav}</div>
-        </StyledGrid>
+        <Container fluid={fluid}>
+          <StyledGrid>
+            <div>{logo}</div>
+            {search}
+            <div>{nav}</div>
+          </StyledGrid>
+        </Container>
       </StyledNavbar>
       <main>{children}</main>
       <StyledBottomNav>{bottomNav}</StyledBottomNav>
