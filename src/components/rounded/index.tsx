a0229f426e6d39be4f2f34449edcae4a00caa542
Okonkwo Henry <henryokonkwo01@gmail.com>
2022-04-22 09:07:17 +0100

    Card update

--- src/components/rounded/index.tsx
+++ src/components/rounded/index.tsx
@@ -1,15 +1,17 @@
 import React, { HTMLAttributes, FC, ReactChild, ReactChildren } from 'react'
 import { StyledRounded } from './styled'
 import { ComponentSize } from '../../tokens/sizes';
+import { PaletteType } from '../../tokens/color';
 
 export interface IRounded {
   children?: ReactChild | ReactChildren | JSX.Element | JSX.Element[];
   borderRadius?: ComponentSize;
+  borderColor?: PaletteType;
 }
 
-const Rounded: FC<IRounded & HTMLAttributes<HTMLDivElement>> = ({children, borderRadius = "default", ...rest}) => {
+const Rounded: FC<IRounded & HTMLAttributes<HTMLDivElement>> = ({children, borderColor, borderRadius = "default", ...rest}) => {
   return (
-    <StyledRounded borderRadius={borderRadius} {...rest}>
+    <StyledRounded borderRadius={borderRadius} borderColor={borderColor} {...rest}>
       {children}
     </StyledRounded>
   )
