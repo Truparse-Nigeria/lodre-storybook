7bc418590a9a8b4097d17c162fdeeb48fbc6f099
Okonkwo Henry <henryokonkwo01@gmail.com>
2022-05-31 22:09:34 +0100

    Fixed radius issue

--- src/components/tabs/styled.ts
+++ src/components/tabs/styled.ts
@@ -45,7 +45,7 @@ export const TabGroup = styled.div<TabsProps>`
   justify-content: ${(props) => (props.centered ? "center" : "flex-start")};
   background: ${(props) =>
     props.background ? PALETTE[props.background] : "transparent"};
-  border-radius: ${(props) => props.radius && "4px"};
+  border-radius: ${(props) => props.radius ? "4px": "0px"};
   overflow-x: auto;
   margin-bottom: ${SIDE_PADDINGS.default}px;
 `;
